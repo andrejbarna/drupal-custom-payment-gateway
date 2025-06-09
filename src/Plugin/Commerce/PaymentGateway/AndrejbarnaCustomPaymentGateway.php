c3e06059909450682ab294bea5ec47a84208643a
an-bar-dev <andrej.barna.development@gmail.com>
2025-06-10 00:42:22 +0200

    feat: Update payment gateway to support new checkout flow and add refund/notification capabilities

--- src/Plugin/Commerce/PaymentGateway/AndrejbarnaCustomPaymentGateway.php
+++ src/Plugin/Commerce/PaymentGateway/AndrejbarnaCustomPaymentGateway.php
@@ -22,6 +22,8 @@ use Drupal\commerce_payment\Plugin\Commerce\PaymentType\PaymentTypeManagerInterf
 use Drupal\commerce_payment\PaymentMethodTypeManager;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\commerce_payment\Attribute\CommercePaymentGateway;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsNotificationsInterface;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsRefundsInterface;
 
 /**
  * Provides the Andrejbarna payment gateway.
@@ -43,7 +45,7 @@ use Drupal\commerce_payment\Attribute\CommercePaymentGateway;
   ],
   requires_billing_information: TRUE,
 )]
-class AndrejbarnaCustomPaymentGateway extends OffsitePaymentGatewayBase {
+class AndrejbarnaCustomPaymentGateway extends OffsitePaymentGatewayBase implements SupportsNotificationsInterface, SupportsRefundsInterface {
 
   /**
    * The number formatter.
@@ -62,8 +64,8 @@ class AndrejbarnaCustomPaymentGateway extends OffsitePaymentGatewayBase {
     EntityTypeManagerInterface $entity_type_manager,
     PaymentTypeManager $payment_type_manager,
     PaymentMethodTypeManager $payment_method_type_manager,
-    NumberFormatter $number_formatter,
-    TimeInterface $time
+    TimeInterface $time,
+    NumberFormatter $number_formatter
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager, $time);
     $this->numberFormatter = $number_formatter;
@@ -80,8 +82,8 @@ class AndrejbarnaCustomPaymentGateway extends OffsitePaymentGatewayBase {
       $container->get('entity_type.manager'),
       $container->get('plugin.manager.commerce_payment_type'),
       $container->get('plugin.manager.commerce_payment_method_type'),
-      $container->get('commerce_price.number_formatter'),
-      $container->get('datetime.time')
+      $container->get('datetime.time'),
+      $container->get('commerce_price.number_formatter')
     );
   }
 
@@ -150,6 +152,7 @@ class AndrejbarnaCustomPaymentGateway extends OffsitePaymentGatewayBase {
       'remote_state' => 'completed',
       'amount' => $order->getTotalPrice(),
       'payment_gateway_mode' => $this->getMode(),
+      'payment_gateway' => $this->parentEntity->id(),
     ]);
     
     $payment->save();
@@ -189,6 +192,80 @@ class AndrejbarnaCustomPaymentGateway extends OffsitePaymentGatewayBase {
     catch (\Exception $e) {
       \Drupal::logger('andrejbarna_custom_payment_gateway')->error('API Error: @message', ['@message' => $e->getMessage()]);
       throw new PaymentGatewayException($this->t('Unable to get rate. Please try again later.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onNotify(Request $request) {
+    // Handle the notification from the payment provider
+    // This is called when the payment provider sends a notification about payment status
+    try {
+      // Get the payment ID from the request
+      $payment_id = $request->query->get('payment_id');
+      if (empty($payment_id)) {
+        throw new PaymentGatewayException('Missing payment ID');
+      }
+
+      // Load the payment
+      $payment = $this->entityTypeManager->getStorage('commerce_payment')->load($payment_id);
+      if (!$payment) {
+        throw new PaymentGatewayException('Invalid payment ID');
+      }
+
+      // Update the payment status based on the notification
+      $remote_status = $request->query->get('status');
+      switch ($remote_status) {
+        case 'completed':
+          $payment->setState('completed');
+          break;
+        case 'failed':
+          $payment->setState('failed');
+          break;
+        default:
+          throw new PaymentGatewayException('Invalid payment status');
+      }
+
+      $payment->setRemoteId($request->query->get('remote_id'));
+      $payment->save();
+    }
+    catch (\Exception $e) {
+      throw new PaymentGatewayException($e->getMessage());
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function refundPayment(PaymentInterface $payment, Price $amount = NULL) {
+    $this->assertPaymentState($payment, ['completed', 'partially_refunded']);
+    // If not specified, refund the entire amount.
+    $amount = $amount ?: $payment->getAmount();
+    $this->assertRefundAmount($payment, $amount);
+
+    try {
+      // Initialize API client
+      $client = new ApiClient($this->configuration['api_key']);
+      
+      // Perform the refund
+      $refund_id = $client->refund($payment->getRemoteId(), $amount->getNumber());
+      
+      $old_refunded_amount = $payment->getRefundedAmount();
+      $new_refunded_amount = $old_refunded_amount->add($amount);
+      
+      if ($new_refunded_amount->lessThan($payment->getAmount())) {
+        $payment->setState('partially_refunded');
+      }
+      else {
+        $payment->setState('refunded');
+      }
+
+      $payment->setRefundedAmount($new_refunded_amount);
+      $payment->save();
+    }
+    catch (\Exception $e) {
+      throw new PaymentGatewayException($e->getMessage());
     }
   }
 } 
--- src/Plugin/WorkflowType/State.php
+++ src/Plugin/WorkflowType/State.php
@@ -5,17 +5,39 @@ namespace Drupal\andrejbarna_custom_payment_gateway\Plugin\WorkflowType;
 use Drupal\workflows\State;
 use Drupal\workflows\StateInterface;
 use Drupal\workflows\WorkflowInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * A state for the payment workflow.
  */
-class State extends State implements StateInterface {
+class PaymentState extends State implements StateInterface {
 
   /**
    * {@inheritdoc}
    */
   public function __construct(WorkflowInterface $workflow, $id, $label, $weight = 0) {
-    parent::__construct($workflow, $id, $label, $weight);
+    parent::__construct($workflow, $id, new TranslatableMarkup($label), $weight);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLabel() {
+    return $this->label;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getWeight() {
+    return $this->weight;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function canTransitionTo($to_state_id) {
+    return $this->workflow->hasTransitionFromStateToState($this->id, $to_state_id);
   }
 
 } 
